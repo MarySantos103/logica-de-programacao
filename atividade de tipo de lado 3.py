298d600fceaeb90d35ad9573b74e1c5fc58fcd3c
MarySantos103 <167312573+MarySantos103@users.noreply.github.com>
2024-05-24 09:02:58 -0300

    Add files via upload

---
+++ atividade de tipo de lado 3.py
@@ -0,0 +1,3 @@
+graus_farenheit=float(input("digite a temperatura em farenheit:"))
+graus_celsius=5*(graus_farenheit-32)/9
+print(f"{graus_farenheit:.2f}graus farenheit correspondem a*f{graus_celsius:2f}grauscelsius")
