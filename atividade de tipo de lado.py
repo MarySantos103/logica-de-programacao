4353dc5047330a33523ce170b08cdca1584dd08e
MarySantos103 <167312573+MarySantos103@users.noreply.github.com>
2024-05-24 09:06:06 -0300

    Add files via upload

---
+++ atividade de tipo de lado.py
@@ -0,0 +1,4 @@
+letlucroPorHora = float(input("quando voce ganha por hora trabalhada?"))
+lethorasdeTrabalho = float(input("quantas horas voce trabalha por mes"))
+letcalculoDoSalario=lucroporhora*horasdetrabalho
+console.log("seu salario no fim do mes e"+calculodosalario)
